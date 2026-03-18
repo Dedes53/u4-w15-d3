07450b7ee0f54a9d5748687e9303310c1991d02e
Dedes <federico.lepore73@gmail.com>
2026-03-18 16:01:58 +0100

    product class

---
+++ src/Product.java
@@ -0,0 +1,34 @@
+public class Product {
+    //attributi
+    final long id;
+    final String name;
+    final String category;
+    final double price;
+
+    // costruttore
+    public Product(long id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    // getters
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+
+}
